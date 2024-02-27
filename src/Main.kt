65348ed3c97d7d09790469031dbe79350df94fd6
Faith-Ndanu <faithndanumunyao@gmail.com>
2024-02-26 23:03:23 -0500

    added content

---
+++ src/Main.kt
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    var x= multiply(8, 500)
+    println(x)
+}
+
+fun multiply(num1: Int, num2: Int): Int{
+    var result = num1 * num2
+    return result
+}
+
